1b2dcaa08be9f017d4f94183590e32e585911b3c
Ovidiu Miu <ovidiu.m94@gmail.com>
2022-04-18 20:13:48 +0300

    import cm-pgn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import { Pgn } from 'cm-pgn';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'pgn-viewer';
